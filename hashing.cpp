19b8f2c1cd5d82bfe3d9476ac0679a8d97db3bf0
Akarsh-Chandrashekar <akarsh.c10@gmail.com>
2021-05-16 17:38:50 -0700

    added pair and equalsum fum array functions

--- hashing.cpp
+++ hashing.cpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <forward_list>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
@@ -65,14 +66,46 @@ vector<int> unionLists(const forward_list<int> &lst1,const forward_list<int> &ls
     return temp;
 }
 
+void pairInArray(int arraySize,int sum,const int arr[],pair<int,int> &paramPair)
+{
+    unordered_set<int> set;
+    for(int i=0;i<arraySize;i++)
+    {
+        int temp=sum-arr[i];
+        auto it=set.find(temp);
+        if(it!=set.end())
+        {
+          paramPair = make_pair((arr[i]),(*it));
+          return;
+        }
+        set.insert(arr[i]);
+    }
+
+}
+
+std::pair<pair<int,int>,pair<int,int>> equalSum(int arr[],int n)
+{
+ unordered_map <int,std::pair<int,int>> map;
+ for(int i=0;i<n;i++)
+ {
+     for(int j=i+1;j<n;j++)
+     {
+        int sum=arr[i]+arr[j];
+        if(map.find(sum)!=map.end())
+        {
+            pair<int,int> temp1=make_pair(arr[i],arr[j]);
+            std::pair<pair<int,int>,pair<int,int>> Temp = make_pair((map.find(sum)->second),temp1);
+            return Temp;
+        }
+        map[sum]=make_pair(arr[i],arr[j]);
+     }
+ }
+}
+
 int main()
 {
-    forward_list<int> l1={1,2,3,4,5,6,7,8};
-    forward_list<int> l2={2,5,6,7,0,9,10};
-
-    vector<int> temp=unionLists(l1,l2);
-    for(auto it=temp.begin();it!=temp.end();it++)
-    {
-        cout<<*it<<endl;
-    }
+    int arr[] = {3, 4, 7, 1, 2, 9, 8};
+    int n  =  sizeof arr / sizeof arr[0];
+    std::pair<pair<int,int>,pair<int,int>> sumEle=equalSum(arr, n);
+    cout<<"{"<<sumEle.first.first<<","<<sumEle.first.second<<","<<sumEle.second.first<<","<<sumEle.second.second<<"}"<<endl;
 }
